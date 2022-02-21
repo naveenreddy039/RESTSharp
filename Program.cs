84f6e29410ab12ef3788068fa7f4bf2fbe0757b2
naveenreddy039 <92319818+naveenreddy039@users.noreply.github.com>
2022-02-21 16:49:13 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RestApi
+{
+    class program
+    {
+        static void Main (string[] args)
+        {
+            Console.WriteLine("Welcome to Rest Api");
+        }
+    }
+}
+
+
